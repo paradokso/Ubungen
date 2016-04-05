f5b6272adee663e99e6d4e2bdaef84083616668a
Paradokso <bublykroman@gmail.com>
2016-04-05 20:39:34 +0200

    validation of HTML - OK.Warnings - I didnt fill out field, because this is a raw draft page

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     		$(this).find('.bar_value').text(progressBarVal + "% ");
 
     		var progressBarWidth = progressBarVal*$element.width()/100;
-    		$(this).animate({ width: progressBarWidth }, 2500);
+    		$(this).animate({ width: progressBarWidth }, progressBarWidth*5);  /* change 'progressBarWidth*5' to 2500 (for example) if we want constant speed */ 
     	})  	
     }
 
@@ -24,58 +24,10 @@ $(document).ready(function() {
         })    
     }
 
-
-
-    progress($('#progressBar'));
-
-
-    $("#myWorks").find('li').not('.active, .open_categories').children('div').hide();
-    $('#openCategory').on('click', 'li', function(e){
-    	e.preventDefault();
-        var activeCategory = $(this).text().substring(0, 3);
-        $(this).siblings().removeClass('active');
-    	$(this).addClass('active');
-        showPortfolio(activeCategory);
-    })
-
-
-    $("#slide_bar").mousedown(function(e){
-        $this=$(this);
-        $this.mousemove(function (){
-            $(this).css({
-                "left": e.pageX - ($(this).width() / 2)
-            });
-            console.log(e.pageX)
-        })
-    })
-
-
-    $(window).scroll(function () {
-        /* parallax effect */ 
-        $(".header").css("background-position","50% " + ($(this).scrollTop() / 1.3) + "px");
-
-
-        $(".twitter").css("background-position","50% " + ($(this).scrollTop() / 10) + "px");
-
-        /*sticky menu*/
-        var headerHight = $(".header").height();
-        if ($(window).scrollTop() > headerHight) {
-             $(".navigation").addClass("fixed").fadeIn();
-             $(".about").css({
-                 "margin-top": $(".navigation").height()
-            })
-        } else {    
-             $(".navigation").removeClass("fixed") ;
-             $(".about").css({
-                 "margin-top": 0
-            });
-        }
-    });
-
     /*SMOOTH SCROLLING to the anchors
      * find the anchor link and smoothly go to choosen id minus height of fixed menu
      */
-    var scroll = function() {
+    (function() {
         $('.navigation').on('click', 'a', function() {
             if (location.pathname.replace(/^\//, '') ==
                 this.pathname.replace(/^\//, '') &&
@@ -93,10 +45,67 @@ $(document).ready(function() {
                 }
             }
         });
-    };
-
-    scroll();
+    })();
 
 
-     
+    
+
+
+    $("#myWorks").find('li').not('.active, .open_categories').children('div').hide();
+    $('#openCategory').on('click', 'li', function(e){
+    	e.preventDefault();
+        var activeCategory = $(this).text().substring(0, 3);
+        $(this).siblings().removeClass('active');
+    	$(this).addClass('active');
+        showPortfolio(activeCategory);
+    })
+
+/*
+    $("#slide_bar").mousedown(function(e){
+        $this=$(this);
+        $this.mousemove(function (){
+            $(this).css({
+                "left": e.pageX - ($(this).width() / 2)
+            });
+            console.log(e.pageX)
+        })
+    })
+*/
+
+    var speed_factor = 1.3;
+    $(window).scroll(function () {
+        /* parallax effect */ 
+        $(".header").css("background-position","50% " + ($(this).scrollTop() / speed_factor) + "px");
+        if ($(window).scrollTop() >= $('.blog_slider').offset().top) {
+            $(".twitter").css("background-position","50% " + (($(this).scrollTop() - $('.twitter').offset().top )/ speed_factor) + "px");
+        } 
+      
+
+        /*sticky menu*/
+        var headerHight = $(".header").height();
+        if ($(window).scrollTop() > headerHight) {
+             $(".navigation").addClass("fixed").fadeIn();
+             $(".about").css({
+                 "margin-top": $(".navigation").height()
+            })
+        } else {    
+             $(".navigation").removeClass("fixed") ;
+             $(".about").css({
+                 "margin-top": 0
+            });
+        }
+/* progress bars of skills*/      
+       if ($(window).scrollTop() > $('.about').offset().top) {   // TODO  - repaire glitch with position
+           progress($('#progressBar'));
+        }; 
+    });
+
+    var numOfCards = $('.blog_slider').children('.blog_card').length;
+    var stepOfSlider = $('.slider_bar>input').prop('max') / numOfCards;
+    $('.slider_bar').on('input change', 'input', function(){
+       $('.blog_slider').css({left : -(stepOfSlider*$(this).val()-30)})
+         
+    })
+    
+    
 });
